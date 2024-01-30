ca9cf2b8476fda4c0f09c7c9a380c33eb627df04
lis <98250889+ThisUserIsSuperCool@users.noreply.github.com>
2024-01-30 14:14:08 +0800

    Update pdf_operator.py

---
+++ pdf_operator.py
@@ -0,0 +1,22 @@
+import PyPDF2
+
+def remove_annotations(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        writer = PyPDF2.PdfFileWriter()
+
+        # Iterate through each page and remove annotations
+        for i in range(reader.getNumPages()):
+            page = reader.getPage(i)
+            if '/Annots' in page:
+                page[PyPDF2.generic.NameObject('/Annots')] = PyPDF2.generic.ArrayObject()
+
+            writer.addPage(page)
+        
+        output_path = pdf_path.split('.')[0]+'_out.pdf'
+
+        with open(output_path, 'wb') as output_file:
+            writer.write(output_file)
+
+if __name__ == '__main__':
+    remove_annotations('sample.pdf')
